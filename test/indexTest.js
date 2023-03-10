1f6e9ee4e37b3a3101b87559bf40ce25aeb1a324
Jacqueline W Sinclair <faycello75@gmail.com>
2023-03-10 02:52:35 -0500

    completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,8 @@ require ( './root.js' );
 
 
 describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+  it('receives one argument and returns it in all caps', function () {
+    
     expect(shout('hello')).toEqual('HELLO');
   })
 })
